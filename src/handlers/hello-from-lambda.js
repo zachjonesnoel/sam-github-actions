cfdf4b8c03f60ef293a847e540fa635946478c22
zachjonesnoel <joneszachariahnoel@gmail.com>
2021-12-04 14:59:08 +0530

    added

--- src/handlers/hello-from-lambda.js
+++ src/handlers/hello-from-lambda.js
@@ -3,7 +3,7 @@
  */
 exports.helloFromLambdaHandler = async () => {
     // If you change this message, you will need to change hello-from-lambda.test.js
-    const message = 'Hello from Lambda!';
+    const message = 'Hello from Lambda!!!';
 
     // All log statements are written to CloudWatch
     console.info(`${message}`);
